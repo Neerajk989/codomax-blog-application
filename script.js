34be8623c5d4721e4e8a1c5eb00138e87a2c2a9e
NEERAJ KHAPRE <neerajk.ml25@sbjit.edu.in>
2026-09-14 13:25:52 +0530

    Use production API path when deployed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? "http://localhost:5000/api" : "/api";
 const TOKEN_KEY = "codomaxToken";
 const USER_KEY = "codomaxUser";
 
